1c9f2adafc79c7e96937656382f53439974c4e80
Poós Dániel <poosdani@gmail.com>
2021-10-13 15:50:03 -0400

    +css +some functions in the js

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 function done(e) {
-    if (checkbox.checked == true) {
-        e.target.classList.toggle('athuzott');
-    }
+    e.target.parentNode.getElementsByTagName('span')[0].classList.toggle('athuzott');
 }
 function del(e) {
-    e.target.remove();
+    e.target.parentNode.remove();
 }
+
 function general() {
-    //document.getElementById('').innerHTML = "";
     let bekert = document.getElementById('field').value;
     if (bekert == '') {
-        alert('ááááááááááááááááááááááááááááááá');
+        alert('You can\'t do nothing!');
         return;
     }
     let li = document.createElement('li');
     li.style.listStyleType = "none";
-    let checkbox = document.getElementsByName('chex');
-    let a = "X";
-    
-    li.innerHTML= '<input type="checkbox" name="chex">'+bekert+a;
-    /*li.addEventListener("click", done);
-    a.addEventListener("click", del);*/
+    let sp1 = document.createElement("span");
+    let sp2 = document.createElement("span");
+    sp1.innerText=bekert;
+    sp2.innerText ="x";
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    li.appendChild(checkbox);
+    li.appendChild(sp1);
+    li.appendChild(sp2);
+    checkbox.addEventListener("click", done);
+    sp2.addEventListener("click", del);
     document.getElementById('lista').appendChild(li);
     document.getElementById('field').value ='';
 }
